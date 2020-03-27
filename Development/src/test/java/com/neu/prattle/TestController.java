c512576f3ee60288069b6da778e8aacb5963a677
Thomas Dohle <dohle.t@husky.neu.edu>
2020-03-26 20:48:21 -0400

    trivial change

--- Development/src/test/java/com/neu/prattle/TestController.java
+++ Development/src/test/java/com/neu/prattle/TestController.java
@@ -4,21 +4,12 @@ import com.neu.prattle.controller.UserController;
 import com.neu.prattle.model.User;
 import com.neu.prattle.service.UserService;
 import com.neu.prattle.service.UserServiceImpl;
-import javax.ws.rs.core.Response;
-import org.apache.http.HttpResponse;
-import org.apache.http.HttpStatus;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.methods.HttpUriRequest;
-import org.apache.http.entity.ContentType;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.codehaus.jackson.map.util.JSONPObject;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import javax.ws.rs.core.Response;
 
 import static org.junit.Assert.assertTrue;
 
@@ -51,3 +42,4 @@ public class TestController {
      Assert.assertEquals(responce2.getStatus(), Response.status(409).build().getStatus());
   }
 }
+
